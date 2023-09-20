87b18d79978d6ec71aea11d1bc388ba6c4d7f620
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-20 23:49:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var inp1 = "Free";var inp2 = "Data";
+function machine(a,b){
+  
+  
+  console.log(" Recieved Inputs are " + inp1 + " & " + inp2);
+  return a+b;
+  
+}
+
+console.log(machine(1,2));
+console.log(machine("Good","Morning"));
